f53e8c45bd11846ec9e356dc41f35c8fe311f81a
Matthew Beech <matthew_beech@live.co.uk>
2021-01-12 21:52:28 +0000

    Added comments to the verify JWT function

--- lib/utils/__test__/jwt.test.ts
+++ lib/utils/__test__/jwt.test.ts
@@ -10,7 +10,7 @@ describe('Unit: JWT Utils', () => {
 
         process.env.JWT_KEY = '1234';
         const memberSince = new Date(Date.now());
-        const data: User = new User({
+        const user: User = new User({
             firstName: 'Joe',
             secondName: 'Bloggs',
             emailAddress: 'joebloggs@email.com',
@@ -26,14 +26,14 @@ describe('Unit: JWT Utils', () => {
         const dateMock = jest.spyOn(Date, 'now').mockReturnValueOnce(1);
 
         // Run test
-        const actual = createJwt(data);
+        const actual = createJwt(user);
 
         // Assert
         expect(actual).toStrictEqual(expected);
 
         // Verify mocks
         expect(jwtMock).toHaveBeenCalledTimes(1);
-        expect(jwtMock).toHaveBeenLastCalledWith({ exp, data }, Config.jwtKey);
+        expect(jwtMock).toHaveBeenLastCalledWith({ user }, Config.jwtKey);
         expect(dateMock).toHaveBeenCalledTimes(1);
     });
 });
--- lib/utils/jwt.ts
+++ lib/utils/jwt.ts
@@ -7,23 +7,26 @@ import { User } from '../models/user';
 const privateKey = fs.readFileSync('test.key');
 const publicKey = fs.readFileSync('test.key.pub');
 
+const options = (emailAddress: string): jwt.SignOptions => ({
+    expiresIn: Number(Config.jwtExpiry),
+    subject: emailAddress,
+    algorithm: 'RS256',
+    issuer: 'MBeech Auth',
+});
+
 /**
  * Create a JSON web token for the user
  * @param user the payload to be included in the JWT
  */
 export function createJwt(user: User) {
-    const options: jwt.SignOptions = {
-        expiresIn: Number(Config.jwtExpiry),
-        subject: user.emailAddress,
-        algorithm: 'RS256',
-        issuer: 'MBeech Auth',
-    };
-
-    const token = jwt.sign({ user }, privateKey, options);
-
+    const token = jwt.sign({ user }, privateKey, options(user.emailAddress));
     return token;
 }
 
+/**
+ * Verify a JWT and create a JWT object
+ * @param jwtString the JWT to verify
+ */
 export function verifyJwt(jwtString: string) {
     const jwtToken = jwt.verify(jwtString, publicKey) as JWT;
     return jwtToken;
